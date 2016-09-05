161bfd28c6a5c881d32abf6372189d1603e1af29
microlandsoftware <microland.mh@gmail.com>
2016-09-05 11:10:20 +0330

    j

--- TestGit/Form1.cs
+++ TestGit/Form1.cs
@@ -15,5 +15,10 @@ namespace TestGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "hello";
+        }
     }
 }
